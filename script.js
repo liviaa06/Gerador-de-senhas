526fc487bd0463ba2da621fb3525aba5fe2d99f6
liviaa06 <128159882+liviaa06@users.noreply.github.com>
2023-10-27 10:36:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,27 +6,30 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-let novaSenha ='';
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
 
-sizePassword.innerHTML =sliderElement.value;
+sizePassword.innerHTML = sliderElement.value;
 
-sliderElement.oninput = function(){
-    sizePassword.innerHTML = this.value;
+slider.oninput = function() {
+  sizePassword.innerHTML = this.value;
 }
+
+
 function generatePassword(){
-    let pass = '';
-    for(let I=0 , n = charset.length; I< sliderElement.value; ++I){
-        pass += charset.charAt(Math.floor(Math.random() * n));
-    }
-    console.log(pass);
-    containerPassword.classList.remove("hide");
-    password.innerHTML = pass;
-    novaSenha = pass;
 
-    function copyPassword(){
-        alert("Senha copiada com sucesso");
-        navegador.clipboard.writeText(novaSenha);
-    }
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
 }
 
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
+}
